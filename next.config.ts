e304649d889979a2d4a094d8033a3bc0991a6f9e
Yakszy1991 <biuro.jakubszymanski@gmail.com>
2025-05-14 14:12:33 +0200

    Fix Next.js native addon loading

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  experimental: { 
+    allowMiddlewareResponseBody: true,
+    turbo: { 
+      loaders: { 
+        '.woff': ['@vercel/font'], 
+        '.woff2': ['@vercel/font'], 
+      }, 
+    },
+  },
 };
 
 export default nextConfig;
